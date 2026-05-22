513a72a182c124136f063a98f2c3d24b60170c59
abelprisca <pria97491@gmail.com>
2026-05-22 11:34:15 +0100

    introduction to javascript varables datatypes

---
+++ index.js
@@ -0,0 +1,27 @@
+let name = "prisca"
+let surname = "Abel"
+
+let fullname = name + surname;
+console.log("my name is " + fullname)
+
+
+let score = 0;
+if(score >= 70){
+    console.log(" you have pass my exams")
+}
+else if (score < 70 ){
+    console.log("You have failed my exams")
+}
+else{
+    console.log("Sorry you are not enligible")
+}
+
+const  person ={
+    name:"Priscilla",
+    age:21,
+    school:"csssGMirnga"
+}
+console.log("this is age " + person.age)
+
+const animal=["Dog", "cat", "cow"]
+console.log(animal[2])
